ea689a1f62cd7f0b7e88141b3ead3d0dbc474fb6
emilybonar <emilykbonar@gmail.com>
2021-03-24 16:12:45 -0500

    Set up project and tailwind jit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	future: {
+		// removeDeprecatedGapUtilities: true,
+		// purgeLayersByDefault: true,
+	},
+	purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
