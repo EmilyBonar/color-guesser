0ba20d60c5dea39f5d1f59f8e432cbace55cd174
emilybonar <emilykbonar@gmail.com>
2021-03-25 16:43:23 -0500

    Basic logic works

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import { RgbColorPicker } from "react-colorful";
-import colors from "./colors";
+import { RgbColor, RgbColorPicker } from "react-colorful";
+import colors, { Color } from "./colors";
 import "./App.css";
 
 function App() {
 	const [color, setColor] = useState({ r: 255, g: 255, b: 255 });
-	const [randomColor] = useState(
+	const [targetColor] = useState(
 		colors[Math.floor(Math.random() * colors.length)],
 	);
-	console.log(randomColor);
 	return (
 		<div
 			className="flex flex-col items-center w-screen h-screen"
@@ -19,7 +18,21 @@ function App() {
 				onChange={setColor}
 				className="w-96 h-96 sm:w-2/3 sm:h-2/3"
 			/>
+			<p className="m-4 text-5xl">{targetColor.name}</p>
+			<button className="p-2 bg-white rounded">Guess</button>
+			<p className="">{grade(color, targetColor).slice(0, 5)}</p>
 		</div>
+	);
+}
+
+function grade(inputColor: RgbColor, targetColor: Color) {
+	return String(
+		100 -
+			(((inputColor.r - targetColor.rgb.r) ** 2 +
+				(inputColor.g - targetColor.rgb.g) ** 2 +
+				(inputColor.b - targetColor.rgb.b) ** 2) /
+				(3 * 255 ** 2)) *
+				100,
 	);
 }
 
--- src/colors.tsx
+++ src/colors.tsx
@@ -1,4 +1,4 @@
-interface Color {
+export interface Color {
 	name: String;
 	rgb: RGB;
 }
