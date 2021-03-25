fcc9f8538ed6b9c432bd4e66ca45b1e8cdd8235d
emilybonar <emilykbonar@gmail.com>
2021-03-24 19:33:44 -0500

    Added color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
+import { RgbColorPicker } from "react-colorful";
+//import "react-colorful/dist/index.css";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
+	const [color, setColor] = useState({ r: 255, g: 255, b: 255 });
+	console.log(color);
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{" | "}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
+		<div
+			className="flex flex-col items-center w-screen h-screen"
+			style={{ backgroundColor: `rgb(${color.r},${color.g},${color.b})` }}
+		>
+			<RgbColorPicker
+				color={color}
+				onChange={setColor}
+				className="w-96 h-96 sm:w-2/3 sm:h-2/3"
+			/>
 		</div>
 	);
 }
