5c2104cc91aa338905068938f0493c98f6d5c274
emilybonar <emilykbonar@gmail.com>
2021-03-26 10:56:14 -0500

    Added menu icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { RgbColor, RgbColorPicker } from "react-colorful";
 import colors, { Color } from "./colors";
+import MenuIcon from "./menu.svg";
 import "./App.css";
 
 function App() {
@@ -14,10 +15,23 @@ function App() {
 			className="flex flex-col items-center w-screen h-screen overflow-hidden"
 			style={{ backgroundColor: `rgb(${color.r},${color.g},${color.b})` }}
 		>
-			<header className="flex w-full bg-black bg-opacity-80">
+			<header className="flex justify-between w-full bg-black bg-opacity-80">
 				<h1 className="p-2 text-4xl font-bold text-transparent from-red-600 via-green-600 to-blue-600 bg-gradient-to-r bg-clip-text">
 					ColorGuesser
 				</h1>
+				<svg
+					className="h-14"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					stroke="white"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<line x1="3" y1="12" x2="21" y2="12"></line>
+					<line x1="3" y1="6" x2="21" y2="6"></line>
+					<line x1="3" y1="18" x2="21" y2="18"></line>
+				</svg>
 			</header>
 			<RgbColorPicker
 				color={color}
