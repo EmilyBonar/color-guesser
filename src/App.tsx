4f81dc559b77959e99980925913f02ad4eb9703e
emilybonar <emilykbonar@gmail.com>
2021-04-01 09:34:17 -0500

    Fix svg props

--- src/App.tsx
+++ src/App.tsx
@@ -74,9 +74,9 @@ function HeaderBar(props: { buttonOnClick: Function }) {
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
 				stroke="white"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			>
 				<line x1="3" y1="12" x2="21" y2="12"></line>
 				<line x1="3" y1="6" x2="21" y2="6"></line>
